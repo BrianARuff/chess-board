0e20cfaefa393c7a18dbafca605189e6f5874a59
Brian Ruff <briananthonyruff@gmail.com>
2021-01-30 00:06:04 -0500

    game is fully functional now, just need to program the rules of the game into the code at this point :)

--- chessBoard.js
+++ chessBoard.js
@@ -26,23 +26,24 @@ window.addEventListener("DOMContentLoaded", () => {
           return e.target.append(piece);
         }
         if (piece.classList[0] === "white" && childPiece.classList[0] === "white" || piece.classList[0] === "black" && childPiece.classList[0] === "black") {
-          return console.log("can't take your own piece!");
+           console.log("can't take your own piece!");
         } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
           console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
           const tempChildParent = childPiece.parentNode;
           childPiece.remove();
           tempChildParent.append(piece);
-
         } else if (!childPiece) {
           console.log("no children found in this sqaure");
-          return;
         } else if (piece.classList[0] === "white" && e.target.classList[0] === "black" 
           || piece.classList[0] === "black" && e.target.classList[0] === "white") {
             console.log("here")
         }
+      } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
+         || piece.classList[0] === "black" && e.target.classList[0] === "white") {
+            const tempParent = e.target.parentNode;
+            e.target.remove();
+            tempParent.append(piece);
       }
-
-      e.target.append(piece);
     });
   });
 });
