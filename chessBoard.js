1557ae644a8db67a8b2beb6e8247bfd7ab41183a
Brian Ruff <briananthonyruff@gmail.com>
2021-01-29 20:09:12 -0500

    chess board piece movement functionality added with letters for pieces (for now)

--- chessBoard.js
+++ chessBoard.js
@@ -2,21 +2,23 @@ console.log('chess');
 
 window.addEventListener("DOMContentLoaded", () => {
   
-  const circle = document.querySelector("#circle");
+  let piece = undefined;
   const squares = document.querySelectorAll("#square");
 
+  squares.forEach((element, i) => {
+    
+    element.addEventListener("dragstart", e => {
+      console.log(e.target);
+      piece = e.target;
+    })
 
-  circle.addEventListener("dragstart", e => {
-    // Add different types of drag data
-  });
-
-  squares.forEach(element => {
     element.addEventListener("dragover", e => {
       e.preventDefault();
-    })
+    });
+
     element.addEventListener("drop", e => {
       console.log("Drag drop");
-      e.currentTarget.append(circle);
+      e.target.append(piece);
     });
   });
 });
