4f6a6ea0140311c99d6ea9f2fba29201988b022b
Brian Ruff <briananthonyruff@gmail.com>
2021-01-30 00:17:24 -0500

    added in turn based functionality (white starts then black is next like in traditional chess)

--- chessBoard.js
+++ chessBoard.js
@@ -1,7 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-
-  let piece = undefined;
-  let PieceOnSquareAlready;
+  let turn = true;
   const squares = document.querySelectorAll("#square");
 
   squares.forEach((element, i) => {
@@ -18,31 +16,62 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log("Drag drop");
       console.log(e);
 
-      // Handle move made in the sqaure but not the piece
-      if (e.target.id && e.target.id === "square") {
-        console.log("square found")
-        const childPiece = e.target.children[0];
-        if (!childPiece) {
-          return e.target.append(piece);
+      if (piece.classList[0] === "white" && turn === true) {
+        console.log("It's whites move");
+        if (e.target.id && e.target.id === "square") {
+          console.log("square found")
+          const childPiece = e.target.children[0];
+          if (!childPiece) {
+            e.target.append(piece);
+          }
+          else if(piece.classList[0] === "white" && childPiece.classList[0] === "white" || piece.classList[0] === "black" && childPiece.classList[0] === "black") {
+             console.log("can't take your own piece!");
+          } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
+            console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
+            const tempChildParent = childPiece.parentNode;
+            childPiece.remove();
+            tempChildParent.append(piece);
+          } else if (!childPiece) {
+            console.log("no children found in this sqaure");
+          } else if (piece.classList[0] === "white" && e.target.classList[0] === "black" 
+            || piece.classList[0] === "black" && e.target.classList[0] === "white") {
+              console.log("here")
+          }
+        } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
+           || piece.classList[0] === "black" && e.target.classList[0] === "white") {
+              const tempParent = e.target.parentNode;
+              e.target.remove();
+              tempParent.append(piece);
         }
-        if (piece.classList[0] === "white" && childPiece.classList[0] === "white" || piece.classList[0] === "black" && childPiece.classList[0] === "black") {
-           console.log("can't take your own piece!");
-        } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
-          console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
-          const tempChildParent = childPiece.parentNode;
-          childPiece.remove();
-          tempChildParent.append(piece);
-        } else if (!childPiece) {
-          console.log("no children found in this sqaure");
-        } else if (piece.classList[0] === "white" && e.target.classList[0] === "black" 
-          || piece.classList[0] === "black" && e.target.classList[0] === "white") {
-            console.log("here")
+        turn = false;
+      } else if (piece.classList[0] === "black" && turn === false) {
+        console.log("It's blacks move");
+        if (e.target.id && e.target.id === "square") {
+          console.log("square found")
+          const childPiece = e.target.children[0];
+          if (!childPiece) {
+            e.target.append(piece);
+          }
+          else if(piece.classList[0] === "white" && childPiece.classList[0] === "white" || piece.classList[0] === "black" && childPiece.classList[0] === "black") {
+             console.log("can't take your own piece!");
+          } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
+            console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
+            const tempChildParent = childPiece.parentNode;
+            childPiece.remove();
+            tempChildParent.append(piece);
+          } else if (!childPiece) {
+            console.log("no children found in this sqaure");
+          } else if (piece.classList[0] === "white" && e.target.classList[0] === "black" 
+            || piece.classList[0] === "black" && e.target.classList[0] === "white") {
+              console.log("here")
+          }
+        } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
+           || piece.classList[0] === "black" && e.target.classList[0] === "white") {
+              const tempParent = e.target.parentNode;
+              e.target.remove();
+              tempParent.append(piece);
         }
-      } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
-         || piece.classList[0] === "black" && e.target.classList[0] === "white") {
-            const tempParent = e.target.parentNode;
-            e.target.remove();
-            tempParent.append(piece);
+        turn = true;
       }
     });
   });
