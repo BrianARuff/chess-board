4f4361478d5fb7bebf7414458e6bdbce845fc440
Brian Ruff <briananthonyruff@gmail.com>
2021-01-30 01:54:36 -0500

    fixed bug that wouldn't allow you to move if you picked up a piece, but put it back down on the same sqaure or if you just clicked it on the same square

--- chessBoard.js
+++ chessBoard.js
@@ -1,11 +1,16 @@
 window.addEventListener("DOMContentLoaded", () => {
   let turn = true;
+  let piece = undefined;
+  let sourceSquare = undefined;
   const squares = document.querySelectorAll("#square");
-
-  squares.forEach((element, i) => {
+  const whoseTurnIsIt = document.querySelector("#whoseTurnIsIt");
+  const whiteCapturedPieces = document.querySelector("#whiteCapturedPieces");
+  const blackCapturedPiieces = document.querySelector("#blackCapturedPieces");
+  squares.forEach((element) => {
 
     element.addEventListener("dragstart", e => {
       piece = e.target;
+      sourceSquare = e.currentTarget.dataset.square;
     });
 
     element.addEventListener("dragover", e => {
@@ -14,12 +19,14 @@ window.addEventListener("DOMContentLoaded", () => {
 
     element.addEventListener("drop", e => {
       console.log("Drag drop");
-      console.log(e);
+      console.log(element);
 
-      if (piece.classList[0] === "white" && turn === true) {
-        console.log("It's whites move");
+      // check if piece was dropped on same square so you don't lose a turn...
+
+      if (piece && piece.classList[0] === "white" && turn === true) {
         if (e.target.id && e.target.id === "square") {
-          console.log("square found")
+          console.log("square found");
+
           const childPiece = e.target.children[0];
           if (!childPiece) {
             e.target.append(piece);
@@ -29,6 +36,7 @@ window.addEventListener("DOMContentLoaded", () => {
           } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
             console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
             const tempChildParent = childPiece.parentNode;
+            whiteCapturedPieces.innerText += (childPiece.innerText + ", ");
             childPiece.remove();
             tempChildParent.append(piece);
           } else if (!childPiece) {
@@ -40,12 +48,19 @@ window.addEventListener("DOMContentLoaded", () => {
         } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
            || piece.classList[0] === "black" && e.target.classList[0] === "white") {
               const tempParent = e.target.parentNode;
+              whiteCapturedPieces.innerText += (e.target.innerText + ", ");
               e.target.remove();
               tempParent.append(piece);
         }
-        turn = false;
+
+        if (element.dataset.square === sourceSquare) {
+          turn = true;
+        } else {
+          whoseTurnIsIt.innerText = "It's Black's Move"
+          turn = false;
+        }
       } else if (piece.classList[0] === "black" && turn === false) {
-        console.log("It's blacks move");
+
         if (e.target.id && e.target.id === "square") {
           console.log("square found")
           const childPiece = e.target.children[0];
@@ -57,6 +72,7 @@ window.addEventListener("DOMContentLoaded", () => {
           } else if ( piece.classList[0] === "white" && childPiece.classList[0] === "black" || piece.classList[0] === "black" && childPiece.classList[0] === "white") {
             console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
             const tempChildParent = childPiece.parentNode;
+            blackCapturedPiieces.innerText += (childPiece.innerText + ", ");
             childPiece.remove();
             tempChildParent.append(piece);
           } else if (!childPiece) {
@@ -68,10 +84,17 @@ window.addEventListener("DOMContentLoaded", () => {
         } else if (piece.classList[0] === "white" && e.target.classList[0] === "black"
            || piece.classList[0] === "black" && e.target.classList[0] === "white") {
               const tempParent = e.target.parentNode;
+              blackCapturedPiieces.innerText += (e.target.innerText + ", ");
               e.target.remove();
               tempParent.append(piece);
         }
-        turn = true;
+
+        if (element.dataset.square === sourceSquare) {
+          turn = false;
+        } else {
+          whoseTurnIsIt.innerText = "It's White's Move"
+          turn = true;
+        }
       }
     });
   });
