d032ca76acd125a57fbc18d38e092dc2b52c8fdb
Brian Ruff <briananthonyruff@gmail.com>
2021-01-31 00:22:08 -0500

    working on some hard bugs with the castling logic and need to fix blacks movement at somepoint

--- chessBoard.js
+++ chessBoard.js
@@ -3,10 +3,27 @@ window.addEventListener("DOMContentLoaded", () => {
   let piece = undefined;
   let sourceSquare = undefined;
   const squares = document.querySelectorAll("#square");
+  const pieces = document.querySelectorAll("#piece");
   const whoseTurnIsIt = document.querySelector("#whoseTurnIsIt");
-  const whiteCapturedPieces = document.querySelector("#whiteCapturedPieces");
-  const blackCapturedPiieces = document.querySelector("#blackCapturedPieces");
+  const whiteCapturedPieces = Array.from(document.querySelector("#whiteCapturedPieces"));
+  const blackCapturedPiieces = Array.from(document.querySelector("#blackCapturedPieces"));
   const moves = [];
+
+  function findPiece(pieceName) {
+    for(let i = 0; i < pieces.length; i++){
+      if (pieceName === pieces[i].dataset.piece) {
+        return pieces[i];
+      }
+    }
+  }
+
+  function findSquare(squareName) {
+    for(let i = 0; i < pieces.length; i++) {
+      if (squareName === pieces[i].parentNode.dataset.square) {
+        return pieces[i].parentNode;
+      }
+    }
+  }
 
   squares.forEach((element) => {
 
@@ -23,75 +40,91 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log("Drag drop");
 
       // check if piece was dropped on same square so you don't lose a turn...
+      if (turn === true) {
+        if (piece.classList[0] === "white" || e.currentTarget.children && e.currentTarget.children[0].classList[0] === "white" || e.currentTarget.children[0].square) {
+          // If target is a square
+          if (e.target.id && e.target.id === "square" || (e.currentTarget.children && e.currentTarget.children[0].classList[0] === "white")) {
+            console.log("square found");
+  
+            const childPiece = e.target.children[0] || e.target;
+  
+            // If trying to castle king side for white
+            const whiteking = findPiece("K1");
+            const whiteKingSource = findSquare(sourceSquare);
+            const whiteKingDestination = findSquare(e.target.parentNode.dataset.square);
+            const g1 = findSquare("g1");
+            const f1 = findSquare("f1");
 
-      if (turn === true && (piece.classList[0] === "white" || (e.currentTarget.children && e.currentTarget.children[0].classList[0] === "white")) || e.currentTarget.children[0].square) {
-        // If target is a square
-        if (e.target.id && e.target.id === "square" || (e.currentTarget.children && e.currentTarget.children[0].classList[0] === "white")) {
-          console.log("square found");
-
-          const childPiece = e.target.children[0] || e.target;
-
-          // If putting piece on empty square
-          if (childPiece.classList.length === 0) {
-            e.target.append(piece);
-            moves.push(piece.dataset.piece + piece.parentNode.dataset.square + ", " + e.target.dataset.square);
-          }
-
-          if (!childPiece) {
-            e.target.append(piece);
-            moves.push(piece.dataset.piece + piece.parentNode.dataset.square + ", " + e.target.dataset.square);
-          }
-          else if (piece.classList[0] === "white" && childPiece.classList[0] === "white") {
-            // Kingside Castling Logic for White
-            if (piece.dataset.piece === "K1" && piece.parentNode.dataset.square === "e1" 
-                && childPiece.parentNode.dataset.square === "g1" 
-                && childPiece.parentNode.nextElementSibling.dataset.square === "h1" 
-                && childPiece.parentNode.nextElementSibling.children[0].dataset.piece === "R1"
-                && !piece.parentNode.nextElementSibling.children[0]) {
-              console.log("King Side Castle Attempt")
-            } else if (
-              piece.dataset.piece === "K1" && piece.parentNode.dataset.square === "e1" 
-                && childPiece.parentNode.dataset.square === "g1" 
-                && childPiece.parentNode.nextElementSibling.dataset.square === "h1" 
-                && childPiece.parentNode.nextElementSibling.children[0].dataset.piece === "R1"
-                && piece.parentNode.nextElementSibling.children[0]
-             ) {
-               console.log("Invalid castling attempt. Something is on sqaure " + piece.parentNode.nextElementSibling.dataset.square)
-             }
-            else {
-              console.log("can't take your own piece!");
-              console.log("")
+            if (whiteKingSource.dataset.square === "e1" && whiteKingDestination.dataset.square === "g1" 
+              && g1.childElementCount === 0 && f1.childElementCount === 0) {
+              moves.push(moves.push(sourceSquare + "," + e.target.dataset.square));
+              e.target.append(whiteking);
+            } else if (e.target.dataset.piece !== "K1" && findSquare(sourceSquare).children[0].dataset.piece !== "K1") {
+              moves.push(sourceSquare + "," + e.target.dataset.square);
+              e.target.append(piece);
+            } else {
+              return
             }
-          } else if (piece.classList[0] === "white" && childPiece.classList[0] === "black") {
-            console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
-            const tempChildParent = childPiece.parentNode;
-            whiteCapturedPieces.innerText += (childPiece.innerText + ", ");
-            childPiece.remove();
-            tempChildParent.append(piece);
-          } else if (piece.classList[0] === "white" && e.target.classList[0] === "black" ||
-            piece.classList[0] === "black" && e.target.classList[0] === "white") {
-            console.log("here")
+  
+            if (!childPiece) {
+              moves.push(sourceSquare + "," + e.target.dataset.square);
+              e.target.append(piece);
+            }
+            else if (piece.classList[0] === "white" && childPiece.classList[0] === "white" || childPiece.children[0].classList[0] === "white") {
+              // Kingside Castling Logic for White
+              if (piece.dataset.piece === "K1" && piece.parentNode.dataset.square === "e1" 
+              && (childPiece.parentNode.dataset.square === "g1" || childPiece.dataset.square === "g1")
+              && (childPiece.parentNode.nextElementSibling.dataset.square === "h1" || childPiece.nextElementSibling.dataset.square === "h1")
+              && (childPiece.parentNode.nextElementSibling.children[0].dataset.piece === "R1" || childPiece.nextElementSibling.children[0].dataset.piece === "R1")
+              && (piece.parentNode.nextElementSibling.children[0]
+              || piece.parentNode.nextElementSibling.nextElementSibling.children[0])) {
+                console.log(`Invalid castling attempt. Something is on sqaure ${piece.parentNode.nextElementSibling.square || 'f1'} or ${piece.parentNode.nextElementSibling.nextElementSibling.dataset.square || 'g1'}`)
+                 return turn = true
+              } else if (
+                piece.dataset.piece === "K1" && (piece.parentNode.dataset.square === "e1" || sourceSquare === "e1") 
+                  && (childPiece.parentNode.dataset.square === "g1" || piece.parentNode.dataset.square === "g1")
+                  && childPiece.nextElementSibling.dataset.square === "h1" 
+                  && childPiece.nextElementSibling.children[0].dataset.piece === "R1"
+                  && piece.parentNode.nextElementSibling.children[0]
+               ) {
+                console.log("King Side Castle Attempt")
+               }
+            } else if (piece.classList[0] === "white" && childPiece.classList[0] === "black") {
+              console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
+              const tempChildParent = childPiece.parentNode;
+              whiteCapturedPieces.innerText += (childPiece.innerText + ", ");
+              moves.push(sourceSquare + "," + e.target.dataset.square);
+              childPiece.remove();
+              tempChildParent.append(piece);
+            } else if (e.target.classList[0] && piece.classList[0] === "white" && e.target.classList[0] === "black") {
+              console.log("line 95??")
+            }
           }
-        } else if (piece.classList[0] === "white" && e.target.classList[0] === "black") {
+      } else if (turn === false) {
+
+      }
+      
+       if (piece.classList[0] === "white" && e.target.classList[0] === "black") {
           const tempParent = e.target.parentNode;
           whiteCapturedPieces.innerText += (e.target.innerText + ", ");
+          moves.push(sourceSquare + "," + e.target.dataset.square);
           e.target.remove();
           tempParent.append(piece);
         }
 
-        if (element.dataset.square === sourceSquare || element.children[0].classList[0] === "white" && e.currentTarget.children[0].classList[0] === "white" ) {
+        if (element.dataset.square === sourceSquare && element.children[0].classList[0] === "white" && e.currentTarget.children[0].classList[0] === "white" ) {
           turn = true;
         } else {
           whoseTurnIsIt.innerText = "It's Black's Move"
           turn = false;
         }
-        // Black Piece Logic Here
-      } else if (piece && e.target && piece.classList[0] === "black" && turn === false) {
-
+        // *** Black Piece Logic Here ***
+      } else if (turn === false && piece.classList[0] === "black") { 
         if (e.target.id && e.target.id === "square" || e.currentTarget.children[0].classList[0] === "black") {
           console.log("square found")
           const childPiece = e.target.children[0];
           if (!childPiece) {
+            moves.push(sourceSquare + "," + e.target.dataset.square);
             e.target.append(piece);
           } else if (piece.classList[0] === "black" && childPiece.classList[0] === "black") {
             console.log("can't take your own piece!");
@@ -100,6 +133,7 @@ window.addEventListener("DOMContentLoaded", () => {
             console.log("opposite pieces on same sqaure", "deletion should occur here", "the attacking piece should be left where the defending piece was");
             const tempChildParent = childPiece.parentNode;
             blackCapturedPiieces.innerText += (childPiece.innerText + ", ");
+            moves.push(sourceSquare + "," + e.target.dataset.square);
             childPiece.remove();
             tempChildParent.append(piece);
           } else if (!childPiece) {
@@ -112,6 +146,7 @@ window.addEventListener("DOMContentLoaded", () => {
           piece.classList[0] === "black" && e.target.classList[0] === "white") {
           const tempParent = e.target.parentNode;
           blackCapturedPiieces.innerText += (e.target.innerText + ", ");
+          moves.push(sourceSquare + "," + e.target.dataset.square);
           e.target.remove();
           tempParent.append(piece);
         }
@@ -123,6 +158,7 @@ window.addEventListener("DOMContentLoaded", () => {
           turn = true;
         }
       }
+      console.log(moves);
     });
   });
 });
